0c8d9566bcf15a1821b2a346ee4736569def9ace
Ulrike Werner <ulrike_werner@web.de>
2022-06-22 18:19:48 +0200

    improved code quality

--- src/components/Activity/styled.js
+++ src/components/Activity/styled.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const ActivityContainer = styled.section`
 	display: grid;
 	grid-template-columns: 1fr 1fr 1fr;
-	border: 1px solid black;
 	margin: 0.3rem;
+	border: 1px solid black;
 	border-radius: 15px;
 `;
 
