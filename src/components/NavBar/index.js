69edd971cb448d5179d18346e575a28d1d67c5e7
Ulrike Werner <ulrike_werner@web.de>
2022-07-05 17:29:12 +0200

    change svgs

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,15 +5,25 @@ import {StyledNavBar} from './style/navStyled';
 
 export default function NavBar() {
 	return (
-		<header>
-			<StyledNavBar>
-				<LinkContainer to="/">
-					<Icon icon="bi:card-list" width="3rem" height="3rem" color="black" />
-				</LinkContainer>
-				<LinkContainer to="/ActiveChallenge">
-					<Icon icon="icon-park-solid:sport" width="3rem" height="3rem" color="black" />
-				</LinkContainer>
-			</StyledNavBar>
-		</header>
+		<StyledNavBar>
+			<LinkContainer to="/" role="LinkToList">
+				<Icon
+					icon="bi:card-list"
+					width="3rem"
+					height="3rem"
+					color="black"
+					alt="list of challenges"
+				/>
+			</LinkContainer>
+			<LinkContainer to="/ActiveChallenge">
+				<Icon
+					icon="icon-park-solid:sport"
+					width="3rem"
+					height="3rem"
+					color="black"
+					alt="active challenge"
+				/>
+			</LinkContainer>
+		</StyledNavBar>
 	);
 }
