12adda07b79541b9a85e78d06cc6366a918d38e4
Ulrike Werner <ulrike_werner@web.de>
2022-07-05 15:26:28 +0200

    add ProgressBox test

---
+++ src/components/ProgressBox/ProgressBox.spec.js
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
+import ProgressBox from './index';
+
+const activities = [
+	{
+		id: 'P_bfsp9qN_5BQnTyPOwBF',
+		challengeId: 'kt3ul4GvkqJpqDcgwBsXy',
+		date: '2022-07-05',
+		distance: 12800,
+		duration: 2592,
+	},
+	{
+		id: 'MAVr4-5xcswZ0KcF5iqd0',
+		challengeId: 'kt3ul4GvkqJpqDcgwBsXy',
+		date: '2022-07-05',
+		distance: 10200,
+		duration: 1883,
+	},
+];
+
+describe('ProgressBox component', () => {
+	it('should render children', () => {
+		render(<ProgressBox distance="5000" activities={activities} />);
+		const totalDistance = screen.getByText(/total distance/);
+		const distance = screen.getByText(/completed distance/);
+		const percent = screen.getByText(/% completed/);
+		const duration = screen.getByText(/duration/);
+
+		expect(totalDistance).toBeInTheDocument();
+		expect(distance).toBeInTheDocument();
+		expect(percent).toBeInTheDocument();
+		expect(duration).toBeInTheDocument();
+	});
+});
