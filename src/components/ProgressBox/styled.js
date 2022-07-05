afe6d363ee7f6601238900f0634da1ba8bd16f94
Ulrike Werner <ulrike_werner@web.de>
2022-07-05 15:14:35 +0200

    add progress function tests

---
+++ src/components/ProgressBox/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ProgressContainer = styled.section`
+	margin-left: 1rem;
+
+	ul {
+		list-style-type: none;
+	}
+`;
+
+export {ProgressContainer};
---
+++ src/utils/progress.js
@@ -0,0 +1,36 @@
+import {secondToDurationData, createDurationString} from './date';
+
+export function sumDuration(activities) {
+	let sum = 0;
+	if (activities.length <= 0) {
+		return sum;
+	}
+
+	activities.forEach(element => {
+		sum += element.duration;
+	});
+
+	return createDurationString(secondToDurationData(sum));
+}
+
+export function sumDistance(activities) {
+	let sum = 0;
+	if (activities.length <= 0) {
+		return '0,00';
+	}
+
+	activities.forEach(element => {
+		sum += element.distance;
+	});
+
+	return sum / 1000;
+}
+
+export function calculatePercent(totalSum, partSum) {
+	let percent = 0;
+	if (partSum === '0,00') {
+		return percent;
+	}
+	percent = Math.round((partSum * 100) / totalSum);
+	return percent;
+}
