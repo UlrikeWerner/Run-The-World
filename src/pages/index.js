905e421c1e6d8d75a836d6f37c60e38925c232cf
Ulrike Werner <ulrike_werner@web.de>
2022-07-11 17:18:52 +0200

    deleted console.log

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,6 @@ export default function ChallengesPage() {
 				return challenge.title.toLowerCase().includes(searchInput.toLowerCase());
 			});
 			setFilterResult(filterChallengeList);
-			console.log(filterResult);
 		} else {
 			setFilterResult(challengeList);
 		}
