8dd6b9a732cd7cc1d28f63dad3a8cb1d6dd94156
Ulrike Werner <ulrike_werner@web.de>
2022-07-16 14:52:28 +0200

    linting

--- src/components/ActivityComponents/Activity/Style/HeadlineSubtitle.js
+++ src/components/ActivityComponents/Activity/Style/HeadlineSubtitle.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const StyledHeadlineSubtitle = styled.h3`
 	margin: 0.6rem 0 0;
 	background: var(--textGradient);
+	/* stylelint-disable property-no-vendor-prefix */
 	-webkit-background-clip: text;
 	-webkit-text-fill-color: transparent;
 	background-clip: text;
