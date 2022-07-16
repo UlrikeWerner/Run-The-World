e1d001b656bbb8e705a60f9e5cc27b28ad07b2bf
UlrikeWerner <UlrikeWerner@users.noreply.github.com>
2022-07-16 15:42:12 +0000

    deploy: 02a5379fcad9c681a240bdce4cd2c47178d381e0

--- main.c4b4510a.iframe.bundle.js
+++ main.c4b4510a.iframe.bundle.js
@@ -5,6 +5,25 @@
 		box-sizing: border-box;
 	}
 
+	:root{
+		--bgColor: #F6F6F6;
+		--lightgreen: #94F284;
+		--green: #68CF57;
+		--darkgreen: #53A646;
+		--palegreen: #e3ffde;
+		--lightgrey: #F4F4F4;
+		--darkgrey: #D4D5D4;
+		--blackTransparent: rgba(0, 0, 0, 0.25); 
+		--greenGradientDiagonal: linear-gradient(274.25deg, var(--lightgreen) 13.83%, var(--darkgreen) 86.17%);
+		--greenReverseGradient: linear-gradient(0deg, var(--lightgreen) 0%, var(--darkgreen) 100%);
+		--dlGreyGradient: linear-gradient(274.25deg, var(--lightgrey) 13.83%, var(--darkgrey) 86.17%);
+		--textGradient: linear-gradient(0deg, var(--darkgreen) 0%, var(--green) 100%);
+		--bgShadow: 5px 5px 4px var(--blackTransparent), -5px -5px 4px white;
+		--activeNavLinkShadow: 3px 3px 4px var(--blackTransparent), -3px -3px 4px white;
+		--buttonBorderColor: white var(--blackTransparent) var(--blackTransparent) white; 
+		--buttonBorderColorInverted:  var(--blackTransparent) white white var(--blackTransparent); 
+	}
+	
 	html {
 		font-size: 16px;
 	}
