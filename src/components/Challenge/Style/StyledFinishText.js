ae3e35f8608681e881ccea2a1b10d4b625ced850
UlrikeWerner <104211944+UlrikeWerner@users.noreply.github.com>
2022-07-17 16:59:30 +0200

    styled challenge cards (#45)

--- src/components/Challenge/Style/StyledFinishText.js
+++ src/components/Challenge/Style/StyledFinishText.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 
 const StyledFinishText = styled.div`
-	position: relative;
-	left: 50%;
+	position: absolute;
+	right: 0;
+	bottom: calc(var(--challengeContainerPadding) * 2.5);
+	left: 0;
 	margin-top: 1rem;
-	transform: translateX(-25%);
-	color: darkred;
+	color: var(--orange);
+	text-align: center;
+	text-transform: uppercase;
+
+	&::before {
+		content: '* completed *';
+	}
 `;
 
 export {StyledFinishText};
--- src/styles.js
+++ src/styles.js
@@ -8,13 +8,15 @@ export const GlobalStyle = createGlobalStyle`
 	}
 
 	:root{
-		--bgColor: #F6F6F6;
+		--offWhite: #F6F6F6;
+		--bgColor: var(--offWhite);
 		--lightgreen: #94F284;
 		--green: #68CF57;
 		--darkgreen: #53A646;
 		--palegreen: #e3ffde;
 		--lightgrey: #F4F4F4;
 		--darkgrey: #D4D5D4;
+		--orange: rgb(255, 159, 64);
 		--blackTransparent: rgba(0, 0, 0, 0.25); 
 		--greenGradientDiagonal: linear-gradient(274.25deg, var(--lightgreen) 13.83%, var(--darkgreen) 86.17%);
 		--greenReverseGradient: linear-gradient(0deg, var(--lightgreen) 0%, var(--darkgreen) 100%);
@@ -23,7 +25,8 @@ export const GlobalStyle = createGlobalStyle`
 		--bgShadow: 5px 5px 4px var(--blackTransparent), -5px -5px 4px white;
 		--activeNavLinkShadow: 3px 3px 4px var(--blackTransparent), -3px -3px 4px white;
 		--buttonBorderColor: white var(--blackTransparent) var(--blackTransparent) white; 
-		--buttonBorderColorInverted:  var(--blackTransparent) white white var(--blackTransparent); 
+		--buttonBorderColorInverted:  var(--blackTransparent) white white var(--blackTransparent);
+		--challengeContainerPadding: 0.375rem;
 	}
 	
 	html {
